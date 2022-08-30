de127c2fab6c7d1c39542dc07891561023d936ce
xunvyre <victoria@thedavids.us>
2022-08-30 13:03:52 -0400

    added parties

--- server.js
+++ server.js
@@ -52,7 +52,11 @@ app.post('/api/candidate', ({ body }, res) =>
 //get all candidates
 app.get('/api/candidates', (req, res) =>
 {   
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                 AS party_name
+                 FROM candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id`;
     db.query(sql, (err, rows) =>
     {
         if (err)
@@ -70,7 +74,12 @@ app.get('/api/candidates', (req, res) =>
 //get a single candidate
 app.get('/api/candidate/:id', (req, res) =>
 {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                 AS party_name 
+                 FROM candidates 
+                 LEFT JOIN parties 
+                 ON candidates.party_id = parties.id 
+                 WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) =>
